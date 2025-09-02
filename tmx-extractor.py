aedd4a44768337e60250b748ab08a0a6a5c75ba2
RaTT <39219347+RaTT19@users.noreply.github.com>
2025-09-02 15:05:06 +0100

    Add files via upload

--- tmx-extractor.py
+++ tmx-extractor.py
@@ -7,59 +7,67 @@ def main():
     # Get inputted files
     inputFiles = sys.argv[1:]
     for inputFile in inputFiles:
-        inputFileName = os.path.basename(inputFile)[:-4]
-        inputSize = os.path.getsize(inputFile)
+        try:
+            inputSize = os.path.getsize(inputFile)
+        except FileNotFoundError:
+            print(inputFile + " cannot be found.")
+            return 0
         
-        with open(inputFile, "rb") as tmxfile:
-            # Get the TMX files offset and the starting offset
-            tmxfile.seek(88)
-            packageFilesInfoOffset = int(flip4ByteEndian(tmxfile.read(4).hex()),16)
-            packageFilesOffset = int(flip4ByteEndian(tmxfile.read(4).hex()),16)
-            
-            # Parse the TMX files info and get the offsets of each currentFile
-            tmxfile.seek(packageFilesInfoOffset)
-            packageFilesInfoString = tmxfile.read(packageFilesOffset - packageFilesInfoOffset).hex()
-            packageFilesInfo = list()
-            while (packageFilesInfoString != ''):
-                # 204e4947 = NIG. I'm sorry
-                if (packageFilesInfoString[:8] == "204e4947"):
-                    fileType = "GIN"
-                else:
-                    fileType = "SNR"
-                    
-                packageFilesInfo += [ { fileType: int(flip4ByteEndian(packageFilesInfoString[16:24]),16) + packageFilesOffset } ]
+        inputFileName, inputFileExt = os.path.splitext(inputFile)
+        
+        if (inputFileExt != ".tmx"):
+            print(inputFile + " is not a tmx file!")
+        else:
+            with open(inputFile, "rb") as tmxFile:
+                # Get the TMX files offset and the starting offset
+                tmxFile.seek(88)
+                packageFilesInfoOffset = int(flip4ByteEndian(tmxFile.read(4).hex()),16)
+                packageFilesOffset = int(flip4ByteEndian(tmxFile.read(4).hex()),16)
                 
-                packageFilesInfoString = packageFilesInfoString[72:]
-            
-            # Extract the files (note the extraction isnt perfect and doesn't account for random 00 padding bytes)
-            for i in range(len(packageFilesInfo)):
-                currentFileInfo = packageFilesInfo[i]
-                nextFileInfo = {}
-                if (i+1 < len(packageFilesInfo)):
-                    nextFileInfo = packageFilesInfo[i+1]
-                    
-                currentFileType =  list(currentFileInfo.keys())[0]
-                
-                currentFileName = inputFileName + "-" + str(i) + "." + currentFileType
-                
-                with open( currentFileName, 'wb') as currentFile:
-                    
-                    tmxfile.seek(currentFileInfo[currentFileType])
-                    
-                    if (nextFileInfo == {}):
-                        currentFileLength = inputSize - currentFileInfo[currentFileType]
+                # Parse the TMX files info and get the offsets of each currentFile
+                tmxFile.seek(packageFilesInfoOffset)
+                packageFilesInfoString = tmxFile.read(packageFilesOffset - packageFilesInfoOffset).hex()
+                packageFilesInfo = list()
+                while (packageFilesInfoString != ''):
+                    # 204e4947 = NIG. I'm sorry
+                    if (packageFilesInfoString[:8] == "204e4947"):
+                        fileType = "GIN"
                     else:
-                        currentFileLength = list(nextFileInfo.values())[0] - currentFileInfo[currentFileType]
+                        fileType = "SNR"
                         
-                    content = bytes.fromhex(tmxfile.read(currentFileLength).hex())
+                    packageFilesInfo += [ { fileType: int(flip4ByteEndian(packageFilesInfoString[16:24]),16) + packageFilesOffset } ]
                     
-                    currentFile.write(content)
-                    
-                currentFile.close()
-                print(currentFileName + " exported!")
+                    packageFilesInfoString = packageFilesInfoString[72:]
                 
+                # Extract the files (note the extraction isnt perfect and doesn't account for random 00 padding bytes)
+                for i in range(len(packageFilesInfo)):
+                    currentFileInfo = packageFilesInfo[i]
+                    nextFileInfo = {}
+                    if (i+1 < len(packageFilesInfo)):
+                        nextFileInfo = packageFilesInfo[i+1]
                         
-        tmxfile.close()
+                    currentFileType =  list(currentFileInfo.keys())[0]
+                    
+                    currentFileName = inputFileName + "-" + str(i) + "." + currentFileType
+                    
+                    with open( currentFileName, 'wb') as currentFile:
+                        
+                        tmxFile.seek(currentFileInfo[currentFileType])
+                        
+                        if (nextFileInfo == {}):
+                            currentFileLength = inputSize - currentFileInfo[currentFileType]
+                        else:
+                            currentFileLength = list(nextFileInfo.values())[0] - currentFileInfo[currentFileType]
+                            
+                        content = bytes.fromhex(tmxFile.read(currentFileLength).hex())
+                        
+                        currentFile.write(content)
+                        
+                    currentFile.close()
+                    print(currentFileName + " exported!")
+                    
+                            
+            tmxFile.close()
 
 
 if __name__=="__main__":
